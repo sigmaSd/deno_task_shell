968fabc5e245cceb39648b67c5c7b9998a2130e0
Nbiba Bedis <bedisnbiba@gmail.com>
2022-12-06 08:19:50 +0100

    Add fuzzing

---
+++ fuzz/fuzz_targets/parse.rs
@@ -0,0 +1,9 @@
+#![no_main]
+use deno_task_shell::parser::parse;
+use libfuzzer_sys::fuzz_target;
+
+fuzz_target!(|data: &[u8]| {
+  if let Ok(s) = std::str::from_utf8(data) {
+    let _ = parse(s);
+  }
+});
